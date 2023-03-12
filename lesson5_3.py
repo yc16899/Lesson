1b7ce1c4f46cefd23b79214734a50d27a4ec2eb8
YC <ychsu16899@gamil.com>
2023-03-12 10:51:02 +0800

    專案架構

--- lesson5_3.py
+++ lesson5_3.py
@@ -1,3 +1,20 @@
 #一個專案內的主執行的py檔，必需要使用__name__的判斷式
+'''
+這是一個猜數字遊戲
+'''
+import random
+
 if __name__ == '__main__':
-    print('這是整個專案內，被python執行的主要執行檔')
+    min = 1
+    max = 10
+    random_value = random.randint(min,max)
+    print('猜數字')
+    while True:
+        keyin = int(input(f'猜數字的範圍{min}~{max}:'))
+        if(keyin == random_value):
+            print(f'猜對了，答案是{random_value}')
+            break
+        else:
+            print('猜錯了')
+
+    print('結束')
