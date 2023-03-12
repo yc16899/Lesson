93dd7a8efc7036f8d777d70b98862b736bcec546
YC <ychsu16899@gamil.com>
2023-03-12 11:25:25 +0800

    自定 function 內的變數

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,9 +1,14 @@
-def sayHello():
+def sayHello():# 沒有參數的 function
     print('Hello')
 
-# 定義一個參數的 function
-def sayHello_withName(name):
+def sayHello_withName(name):# 定義一個參數的 function
     print(f'Hello {name}')
 
+def square_area(side): # 定義一個參數
+    area = side ** 2 # function 內的變數
+    return area
+
 if __name__ == '__main__':
-    sayHello_withName('yc')
+    side = eval(input('請輸入一個正方形的邊：'))
+    area = square_area(side) # 文件變數
+    print(f'正方形，一邊為10，面積為{area}')
